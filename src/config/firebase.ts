970b0fd8fbd8d42ce142b75044068c99a378eadd
tianyi1580 <tianyi1580@gmail.com>
2026-02-24 15:30:33 -0500

    Connected firebase backend

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT.firebaseapp.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
+    apiKey: "AIzaSyDhSGakyGYW4xGtSE7ny6IW95ZssytmYms",
+    authDomain: "who-are-you-again.firebaseapp.com",
+    projectId: "who-are-you-again",
+    storageBucket: "who-are-you-again.firebasestorage.app",
+    messagingSenderId: "419609342903",
+    appId: "1:419609342903:web:c2fe54d6af0c23f25dee32",
+    measurementId: "G-9ETBYF5YHQ",
 };
 
 const app = initializeApp(firebaseConfig);
